e724771b2d67af77baf331934c2c5d57f5f2412c
Frank Aka <s00268038@atu.ie>
2025-04-17 10:52:54 +0100

    removed secure bridge connection (not compatible with yun)

--- iotproj/iotproj.ino
+++ iotproj/iotproj.ino
@@ -1,7 +1,7 @@
 #include <Bridge.h>
 #include <HttpClient.h>
 #include <Process.h>
-#include <BridgeSSLClient.h>
+
 
 const int soundSensor = A0;
 const int ledPin = 5;
@@ -26,23 +26,26 @@ void loop()
 
   if (soundLevel > threshold) 
   {
-    Serial.println(Alert! Siren detected!");
-    digitalWrite(led, HIGH);
+    Serial.println("Alert! Siren detected!");
+    digitalWrite(ledPin, HIGH);
     digitalWrite(buzzerPin, HIGH);
   }
+  
 }
 
 void getWeather() 
 {
-  BridgeSSLClient sslClient;
-  HttpClient client(sslClient);
+  HttpClient client;
 
   String host = "api.openweathermap.org";
   String apiKey = "efe02b156f1fe98a7b88d2547444728a";  
   String city = "Sligo,IE";
   String url = "/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=metric";
+  boolean isReading = false;
+  String weatherData = "";
 
-  client.get(host, 443, url);
-
+  client.get(host, 80, url);
   Serial.println("Fetching weather data");
+
+  
 }
